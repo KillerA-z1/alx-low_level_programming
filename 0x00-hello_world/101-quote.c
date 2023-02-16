46f2f657277525ec85eb1044fca922f3ae25f811
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-02-16 15:17:41 -0800

    advanced Hello, World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - A c programming that prints a line to the standard error
+ *Code by Yohannes
+ * Return: 1 (success)
+*/
+
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
