87943a2b543fb17d9c25cb4bf6f3b87443bbb787
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-02-17 14:40:50 -0800

    numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ *main - Entery point
+ *Description:print all single digit numbers of base 10
+ *Return:Always 0
+*/
+int main(void)
+{
+	int n;
+
+	while (n < 10)
+	{
+	printf("%d", n);
+	n++;
+	}
+	printf("\n");
+	return (0);
+}
