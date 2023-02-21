9f4e620f30e0821cf1ee24e7822f7718231b93c3
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-02-21 13:19:10 -0800

    adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Entery point
+ * @num1: integer 1
+ * @num2: integer 2
+ * Description: accept two integers and returns sum
+ * Return: sum
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+}
