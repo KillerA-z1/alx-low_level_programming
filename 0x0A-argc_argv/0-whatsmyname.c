22ee1de15b50ea4421add2735a2fb0e8f6f4f8da
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-03-10 03:42:31 -0800

    program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
+#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -10,6 +10,8 @@
  */
 int main(int argc, char *argv[])
 {
+	(void)argc;
+
 	printf("%s\n", argv[0]);
 	return (0);
 }
