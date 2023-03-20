bad201b3bde28a41b88c5670ee185c41e0eb5f89
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-03-20 11:45:50 -0700

    Define struct dog with name,age and owner

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/*
+ * File: dog.h
+ * Desc: Header file that defines a new type struct dog.
+ */
+
+/**
+ * struct dog - A new type describing a dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
