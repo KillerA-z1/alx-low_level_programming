bc492683a3369091a288be96c6e416cd7200ff53
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-03-20 12:44:47 -0700

    Defineing dog_t as a new name type for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,6 +20,9 @@ struct dog
 };
 /*-------------------------------------------------------------------*/
 
+typedef struct dog dog_t;
+
+/*********************************************************************/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
