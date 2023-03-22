ae27bdcbd3aa6f9fb35b5fd99244cb205bc4b48a
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-03-22 13:58:26 -0700

    function that executes a function given element of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Iterates through an array and calls a function
+ * @array: Array to iterate
+ * @size: size of the array
+ * @action: Pointer to a function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
