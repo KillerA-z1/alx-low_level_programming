cd688112e67316606b67860896b48d5b06d4123f
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-03-31 14:11:37 -0700

    function that prints before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+void __attribute__((constructor)) first(void);
+
+/**
+ * first - Prints a string before the
+ *         main function is executed.
+ */
+void first(void)
+{
+printf("You're beat! and yet, you must allow,\n"
+	"I bore my house upon my back!\n");
+}
+
