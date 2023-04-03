b9983aa3b6714f21c25218d2700d45f12d8f0c7b
KillerA-z1 <ahmedz1a2m@gmail.com>
2023-04-03 14:23:21 -0700

    Hot fix - to  keeping the original value of h unchanged

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,19 +5,18 @@
  * Return: its returns i the number of elements in lisiint.
  */
 
-
-
-
-
 size_t print_listint(const listint_t *h)
 
 {
 	size_t i;
+	const listint_t *current = h;
+	/* This new pointer is then used to traverse the linked list */
+	/* while keeping the original value of h unchanged*/
 
-	for (i = 0; h != NULL; i++)
+	for (i = 0; current != NULL; i++)
 	{
-	printf("%d\n", h->n);
-	h = h->next;
+	printf("%d\n", current->n);
+	current = current->next;
 	}
 
 return (i);
