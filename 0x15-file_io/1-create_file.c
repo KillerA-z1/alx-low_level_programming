9a353a5f02126b67b1d0878127779f0d9c55f6b8
KillerA-z1 <zeoof@DESKTOP-VUTJ4PI.localdomain>
2023-05-09 02:55:38 +0300

    hot fix 00

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, len = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
